feea65a508cea3b31b98bbbf91efeda720f233d8
Jojo <monMail@mail.com>
2022-09-05 10:14:58 +0200

    création projet, blade,layout, partials, controller, route  et installation bootstrap

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite(['resources/js/app.js'])
+   <!-- Bootstrap core CSS -->
+   <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+
+   <!-- Custom fonts for this template -->
+   <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+ 
+   <!-- Custom styles for this template -->
+   <link href="{{asset('css/business-casual.min.css')}}" rel="stylesheet">
+</head>
+<body>
+    @yield("content")
+
+     <!-- Bootstrap core JavaScript -->
+  <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
+  <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\StoreController;
+use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [WelcomeController::class, 'index']);
+
+Route::get('/about', [AboutController::class, 'index'])->name('about');
+
+Route::get('/products', [ProductsController::class, 'index'])->name('prod');
+
+Route::get('/', [StoreController::class, 'index'])->name('store');
